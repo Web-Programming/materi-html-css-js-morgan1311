0cbafbaec27d9cb54db427415903fd7c0b0e0e79
shaddiq131 <muhammadammarshaddiq_2428240077@mhs.mdp.ac.id>
2026-04-01 14:07:56 +0700

    JavaScript

--- Pertemuan9/script.js
+++ Pertemuan9/script.js
@@ -1,45 +1,46 @@
- //fungsi untuk memuat Data dari session storage dan menampilkannya
- function displayGuest(){
-    const guestListUI = document.getElementById('guestList');
-    guestListUI.innerHTML = ''; //reset tampilan list
+// Fungsi untuk memuat data dari session storage dan menampilkannya
+function displayGuests() {
+  const guestListUI = document.getElementById('guestList');
+  guestListUI.innerHTML = ''; // Reset tampilan list
 
-    //mengambil data dari session storage, jika kosong buat array baru
-    let guest = JSON.parse(sessionStorage.getItem('guest')) || [];
+  // Mengambil data dari session storage, jika kosong buat array baru
+  let guests = JSON.parse(sessionStorage.getItem('guests')) || [];
 
-    guests.forEach((guest)=> {
-        let li = document.createElement('li');
-        li.textContent = guest;
-        guestListUI.appendChild(li);
-    });
- }
- 
-//fungsi untuk menambahkan data
-function addGuest() {
-    const input = document.getElementById('guestInput');
-    const guestName = input.value;
-    if(guestName === ''){
-        alert('nama tidak boleh kosong');
-        return;
-    }
-    
-    //ambil data lama
-    let guest = JSON.parse(sessionStorage.getItem('guest')) || [];
-    guest.push(guestName);
-
-    //simpan kembali ke storage dalam bentuk string
-    sessionStorage.setIte('guest', JSON.stringify(guest));
-    //bersijkan input dan perbarui tampilan
-    input.value = '';
-    displayGuest();
-
-    //fungsi untuk menghapus semua data 
-    function clearList(){
-        if(confirm('Hapus Semua data di sesi ini?')){
-            sessionStorage.removeItem('guest');
-            displayGuest(); 
-        }
-    }
-
-//jalankan fungsi display saay pertama kali dimuat(dibuka)
-window.onload = displayGuest
+  guests.forEach((guest) => {
+    let li = document.createElement('li');
+    li.textContent = guest;
+    guestListUI.appendChild(li);
+  });
 }
+
+// Fungsi untuk menambah data
+function addGuest() {
+  const input = document.getElementById('guestInput');
+  const guestName = input.value;
+  if (guestName === '') {
+    alert('Nama tidak boleh kosong!');
+    return;
+  }
+
+  // Ambil data lama
+  let guests = JSON.parse(sessionStorage.getItem('guests')) || [];
+  // Tambah data baru ke array
+  guests.push(guestName);
+
+  // Simpan kembali ke sstorage dalam bentuk string
+  sessionStorage.setItem('guests', JSON.stringify(guests));
+  // Bersihkan input dan perbarui tampilan
+  input.value = '';
+  displayGuests();
+}
+
+// Fungsi untuk menghapus semua data
+function clearList() {
+  if (confirm('Hapus semua data tamu di sesi ini?')) {
+    sessionStorage.removeItem('guests');
+    displayGuests();
+  }
+}
+
+// Jalankan fungsi display saat halaman pertama kali dimuat
+window.onload = displayGuests;
