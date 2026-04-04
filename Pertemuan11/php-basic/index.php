38ccbdfc28830ac99c5b2eb30d8dc177924a24a1
shaddiq131 <muhammadammarshaddiq_2428240077@mhs.mdp.ac.id>
2026-04-04 11:13:50 +0700

    Php

---
+++ Pertemuan11/php-basic/index.php
@@ -0,0 +1,13 @@
+<?php
+// Variabel
+$nama = "Morgan Alan";
+$umur = 21;
+
+// Konstanta
+define("KAMPUS", "Universitas Multi Data Palembang");
+
+// Output
+echo "Nama: $nama <br>";
+echo "Umur: $umur <br>";
+echo "Kampus: " . KAMPUS;
+?>
